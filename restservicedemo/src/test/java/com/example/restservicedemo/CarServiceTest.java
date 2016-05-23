740ffcb1302ca33f405b29fae5d0774deab29145
Maciej Pietras <Maciej Pietras>
2016-05-23 12:52:14 +0200

    Dodanie car manager i REST service

--- restservicedemo/src/test/java/com/example/restservicedemo/CarServiceTest.java
+++ restservicedemo/src/test/java/com/example/restservicedemo/CarServiceTest.java
@@ -1,11 +1,14 @@
 package com.example.restservicedemo;
 
+import static com.jayway.restassured.RestAssured.delete;
 import static com.jayway.restassured.RestAssured.get;
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
+
+import javax.ws.rs.core.MediaType;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,6 +18,8 @@ import com.jayway.restassured.RestAssured;
 
 public class CarServiceTest {
 	
+	public static final String CAR_MODEL = "Mustang";
+	
 	@BeforeClass
 	public static void setUp(){
 		RestAssured.baseURI = "http://localhost";
@@ -23,23 +28,42 @@ public class CarServiceTest {
 	}
 	
 	@Test
-	public void getCar(){
-		get("/car/0").then().assertThat().body("model", equalTo("Corsa"));
+	public void addCars(){		
 		
-		Car aCar = get("/car/0").as(Car.class);
-		assertThat(aCar.getMake(), equalToIgnoringCase("Opel"));
+		delete("/car/").then().assertThat().statusCode(200);
+		
+		Car car = new Car(1L, "Ford", "Mustang", 1953);
+		
+		given().contentType(MediaType.APPLICATION_JSON).body(car).
+	    when().post("/car/").
+	    then().assertThat().statusCode(201);
+				
+		Car rCar = get("/car/1").as(Car.class);
+		
+		assertThat("Mustang", equalToIgnoringCase(rCar.getModel()));
+		
 	}
 	
-	@Test
-	public void addCar(){
-		
-		Car aCar = new Car(2, "Ford", "Fiesta", 2011);
-		given().
-		       contentType("application/json").
-		       body(aCar).
-		when().	     
-		post("/car/").then().assertThat().statusCode(201).body(containsString("Car saved:"));
-	}
+}
+	
+//	@Test
+//	public void getCar(){
+//		get("/car/0").then().assertThat().body("model", equalTo("Corsa"));
+//		
+//		Car aCar = get("/car/0").as(Car.class);
+//		assertThat(aCar.getMake(), equalToIgnoringCase("Opel"));
+//	}
+//	
+//	@Test
+//	public void addCar(){
+//		
+//		Car aCar = new Car(2, "Ford", "Fiesta", 2011);
+//		given().
+//		       contentType("application/json").
+//		       body(aCar).
+//		when().	     
+//		post("/car/").then().assertThat().statusCode(201).body(containsString("Car saved:"));
+//	}
 	
 
-}
+
